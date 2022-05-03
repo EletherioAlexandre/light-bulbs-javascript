074c03e87b296f3cef63a8488fa1fc847b8ee92e
Alexandre Junior <alexandre.junior@impar.com.br>
2022-05-03 20:16:29 -0300

    v1.0.0 of Light Bulbs

--- src/app.js
+++ src/app.js
@@ -1,23 +1,45 @@
-const TURN_ON = document.getElementById("switch-On");
-const TURN_OFF = document.getElementById("switch-Off");
-const BULB = document.getElementById("img-src");
-const ELEMENT = document.querySelector(".body");
-const SKY = document.getElementById("sky");
+const MAIN_TOGGLE = document.querySelector("#main-toggle");
+let ISACTIVE = false;
 
-const switchOn = () => {
-  BULB.src = "../assets/light-bulbOn.png";
-  ELEMENT.style.backgroundColor = "#000";
-  ELEMENT.style.color = "#fff";
+const interruptor = () => {
+  const TURN_ON = document.getElementById("switch-On");
+  const TURN_OFF = document.getElementById("switch-Off");
+  const BULB = document.getElementById("img-src");
+  const ELEMENT = document.querySelector(".body");
+
+  if (ISACTIVE) {
+    const switchOn = () => {
+      BULB.src = "../assets/light-bulbOn.png";
+      ELEMENT.style.backgroundColor = "#000";
+      ELEMENT.style.color = "#fff";
+    };
+    const switchOff = () => {
+      BULB.src = "../assets/light-bulbOff.png";
+      ELEMENT.style.backgroundColor = "rgb(240 238 220)";
+      ELEMENT.style.color = "#000";
+    };
+
+    const breakLightBulb = () => {
+      BULB.src = "../assets/light-bulbBroken.jpg";
+      ELEMENT.style.backgroundColor = "#fff";
+      ELEMENT.style.color = "#000";
+    };
+
+    TURN_ON.addEventListener("click", switchOn);
+    TURN_OFF.addEventListener("click", switchOff);
+    BULB.addEventListener("dblclick", breakLightBulb);
+
+    MAIN_TOGGLE.src = "../assets/on-main-toggle.png";
+    ISACTIVE = false;
+    document.getElementById("switch-On").disabled = false;
+    document.getElementById("switch-Off").disabled = false;
+  } else {
+    document.getElementById("switch-On").disabled = true;
+    document.getElementById("switch-Off").disabled = true;
+    MAIN_TOGGLE.src = "../assets/off-main-toggle.png";
+    MAIN_TOGGLE.addEventListener("click", interruptor);
+    ISACTIVE = true;
+  }
 };
 
-const switchOff = () => {
-  BULB.src = "../assets/light-bulbOff.png";
-
-  ELEMENT.style.backgroundColor = "rgb(240 238 220)";
-  ELEMENT.style.color = "#000";
-};
-
-TURN_ON.addEventListener("click", switchOn);
-TURN_OFF.addEventListener("click", switchOff);
-BULB.addEventListener("mouseover", switchOn);
-BULB.addEventListener("mouseleave", switchOff);
+MAIN_TOGGLE.onclick(interruptor());
